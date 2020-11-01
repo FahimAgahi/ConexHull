46eca3968770fded55ea6d9c5bd9c7e618b15bd1
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-11-02 00:50:11 +0330

    Drawing Lines | Blind Search

--- src/org/bihe/main/PointPanel.java
+++ src/org/bihe/main/PointPanel.java
@@ -4,12 +4,16 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.List;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -30,7 +34,71 @@ public class PointPanel extends JPanel implements MouseListener {
 				g2d.drawString("(" + point.x + "," + point.y + ")", (point.x), (point.y) + 20);
 			}
 		}
+	}
 
+	public void drawConvexWithBlindSearch() {
+		ArrayList<Point> innerPoints = new ArrayList<>();
+		for (Point q : points) {
+			for (Point p1 : points) {
+				for (Point p2 : points) {
+					for (Point p3 : points) {
+						if (angleTurn(p1, p2, q) < 0 && angleTurn(p2, p3, q) < 0 && angleTurn(p3, p1, q) < 0) {
+							innerPoints.add(q);
+						}
+					}
+				}
+			}
+
+		}
+
+		Graphics2D g2d = (Graphics2D) this.getGraphics();
+		ArrayList<Point> convexPoints = new ArrayList();
+		convexPoints.addAll(points);
+		convexPoints.removeAll(innerPoints);
+		
+		g2d.setPaint(Color.RED);
+		for (Point point : convexPoints) {
+			g2d.fillOval((int) point.getX(), (int) point.getY(), 5, 5);
+			if (showXY == true) {
+				g2d.drawString("(" + point.x + "," + point.y + ")", (point.x), (point.y) + 20);
+			}
+		}
+		// sort
+		sortByAngles(convexPoints);
+		drawConvexHull(convexPoints);
+
+	}
+
+	public void drawConvexHull(ArrayList<Point> extrems) {
+		Graphics g = this.getGraphics();
+		g.setColor(Color.RED);
+		for (Point p : extrems) {
+			g.fillOval(p.x - 5, p.y - 5 , 10, 10);
+		}
+
+		for (int i = 0; i < extrems.size() - 1; i++) {
+			((Graphics2D) g).setStroke(new BasicStroke(3));
+			Point p1 = extrems.get(i);
+			Point p2 = extrems.get(i + 1);
+			g.drawLine((int) p1.getX(), (int) p1.getY(), (int) p2.getX(), (int) p2.getY());
+		}
+		Point p1 = extrems.get(extrems.size() - 1);
+		Point p2 = extrems.get(0);
+		g.drawLine((int) p1.getX(), (int) p1.getY(), (int) p2.getX(), (int) p2.getY());
+	}
+
+	public double angleTurn(Point p1, Point p2, Point p3) {
+		return (double) (p2.getX() - p1.getX()) * (p3.getY() - p1.getY())
+				- (p3.getX() - p1.getX()) * (p2.getY() - p1.getY());
+
+	}
+	
+	public void sortByAngles(ArrayList<Point> convexPoints) {
+		Collections.sort(convexPoints, Comparator.comparing(Point :: getY).reversed().thenComparing(Point ::getX));
+		
+		Collections.sort(convexPoints, Comparator.comparing(
+				x -> Math.atan2(x.getX() - convexPoints.get(0).getX(), x.getY() - convexPoints.get(0).getY())));
+	
 	}
 
 	@Override
@@ -39,10 +107,13 @@ public class PointPanel extends JPanel implements MouseListener {
 		super.paintComponent(g);
 		doDrawing(g);
 	}
-
+	public void clear() {
+		this.points.clear();
+		repaint();
+		
+	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
 		points.add(e.getPoint());
 		repaint();
 	}
--- src/org/bihe/main/mainPanel.java
+++ src/org/bihe/main/mainPanel.java
@@ -1,6 +1,7 @@
 package org.bihe.main;
 
 import java.awt.EventQueue;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,10 +16,12 @@ import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.JLabel;
 import org.bihe.main.PointPanel;
+import javax.swing.ButtonGroup;
 
 public class mainPanel {
 
-	private JFrame frame;
+	private JFrame frmConvexhull;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -28,7 +31,7 @@ public class mainPanel {
 			public void run() {
 				try {
 					mainPanel window = new mainPanel();
-					window.frame.setVisible(true);
+					window.frmConvexhull.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -47,36 +50,36 @@ public class mainPanel {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 555, 429);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmConvexhull = new JFrame();
+		frmConvexhull.setTitle("Convexhull");
+		frmConvexhull.setResizable(false);
+		frmConvexhull.setBounds(100, 100, 555, 429);
+		frmConvexhull.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
 
 		panel.setBackground(new Color(191, 230, 244));
 		// 184, 229, 245
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		frmConvexhull.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 
 		JRadioButton rdbtnBlindSearch = new JRadioButton("Blind Search ");
+		buttonGroup.add(rdbtnBlindSearch);
 		rdbtnBlindSearch.setBounds(419, 32, 112, 23);
 		rdbtnBlindSearch.setBackground(new Color(191, 230, 244));
 		panel.add(rdbtnBlindSearch);
 
 		JRadioButton rdbtnGrahamsScan = new JRadioButton("Graham Scan");
+		buttonGroup.add(rdbtnGrahamsScan);
 		rdbtnGrahamsScan.setBounds(419, 102, 123, 23);
 		rdbtnGrahamsScan.setBackground(new Color(191, 230, 244));
 		panel.add(rdbtnGrahamsScan);
 
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Quick Hull");
+		buttonGroup.add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setBounds(419, 67, 99, 23);
 		rdbtnNewRadioButton.setBackground(new Color(191, 230, 244));
 		panel.add(rdbtnNewRadioButton);
-
-		JButton btnApply = new JButton("Apply");
-		btnApply.setBounds(429, 146, 102, 29);
-		panel.add(btnApply);
 
 		PointPanel panel_1 = new PointPanel();
 		panel_1.setBackground(Color.WHITE);
@@ -92,16 +95,50 @@ public class mainPanel {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
+				
 				panel_1.showXY = chckbxShowX.isSelected();
 				panel_1.repaint();
 
 			}
 		});
-
+		
+		//apply
+		JButton btnApply = new JButton("Apply");
+		btnApply.setBounds(429, 146, 102, 29);
+		
+		btnApply.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(rdbtnBlindSearch.isSelected()) {
+					panel_1.drawConvexWithBlindSearch();
+				}else if(rdbtnGrahamsScan.isSelected()) {
+					//Grahams Scan
+				}else if(rdbtnNewRadioButton.isSelected()) {
+					//Quick hull
+				}
+				
+			}
+		});
+		panel.add(btnApply);
+		
+		//clear
+		JButton btnClear = new JButton("Clear");
+		btnClear.setBounds(429, 190, 102, 29);
+		
+		btnClear.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				panel_1.clear();
+				
+			}
+		});
+		panel.add(btnClear);
+		
 		JLabel lblNewLabel = new JLabel("Click on the square above to draw a point");
 		lblNewLabel.setForeground(new Color(119, 136, 153));
-		lblNewLabel.setBounds(75, 382, 268, 16);
+		lblNewLabel.setBounds(75, 383, 268, 16);
 		panel.add(lblNewLabel);
 
 	}
